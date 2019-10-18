2219b895c0999ff489ef5d2f090c39a8b4ce04cf
antoniooreany <antonio_oreany@mail.ru>
2019-10-18 13:49:26 +0200

    Coder.decode() is implemented

--- src/main/java/com/coder/Coder.java
+++ src/main/java/com/coder/Coder.java
@@ -2,4 +2,33 @@ package main.java.com.coder;
 
 public class Coder {
 
+    private static final char A_LOWER_CASE = 'a';
+    private static final char Z_LOWER_CASE = 'z';
+    private static final char A_UPPER_CASE = 'A';
+    private static final char Z_UPPER_CASE = 'Z';
+
+    public static String encode(String in, int rot) {
+        char[] inChars = in.toCharArray();
+        char[] outChars = new char[inChars.length];
+        for (int i = 0; i < inChars.length; i++) {
+            if ((inChars[i] >= A_LOWER_CASE && inChars[i] <= Z_LOWER_CASE)) {
+                decode(rot, inChars, outChars, i, A_LOWER_CASE, Z_LOWER_CASE);
+            } else {
+                if (inChars[i] >= A_UPPER_CASE && inChars[i] <= Z_UPPER_CASE) {
+                    decode(rot, inChars, outChars, i, A_UPPER_CASE, Z_UPPER_CASE);
+                } else {
+                    outChars[i] = inChars[i];
+                }
+            }
+        }
+        return new String(outChars);
+    }
+
+    private static void decode(int rot, char[] inChars, char[] outChars, int i, char a, char z) {
+        outChars[i] = (char) (inChars[i] + rot);
+        int absoluteOut = outChars[i] - a;
+        int divider = z - a;
+        int threshold = a - absoluteOut / divider;
+        outChars[i] = (char) (threshold + absoluteOut % divider);
+    }
 }
--- src/main/java/com/coder/Main.java
+++ src/main/java/com/coder/Main.java
@@ -2,6 +2,44 @@ package main.java.com.coder;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello!");
+
+        String e = Coder.encode("e", 5);
+        System.out.println(e);
+
+        String e1 = Coder.encode("z", 1);
+        System.out.println(e1);
+
+        int rot = 1 + ('z' - 'a' + 1) * 22;
+        System.out.println(rot);
+        String e2 = Coder.encode("z", rot);
+        System.out.println(e2);
+
+        int rot_ = 2 + ('z' - 'a' + 1) * 23;
+        System.out.println(rot_ + "  // char: overflow");
+        String e2_ = Coder.encode("z", rot_); // char: overflow
+        System.out.println(e2_ + "  // char: overflow");
+
+        String e3 = Coder.encode("X", 7);
+        System.out.println(e3);
+
+        int rot1 = 7 + ('Z' - 'A' + 1) * 19;
+        System.out.println(rot1);
+        String e4 = Coder.encode("X", rot1);
+        System.out.println(e4);
+
+        int _rot1 = 7 + ('Z' - 'A' + 1) * 20;
+        System.out.println(_rot1 + "    // char: overflow");
+        String _e4 = Coder.encode("X", _rot1); // char: overflow
+        System.out.println(_e4 + "    // char: overflow");
+
+
+        System.out.println("/////////////////////////////////////////////");
+
+
+
+
+
+
+
     }
 }
